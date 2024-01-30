90e8d0de2d7202717215a684a102a1d9f41ec267
de//con <88671202+AIc1n@users.noreply.github.com>
2024-01-30 04:52:55 +0300

    Add files via upload

---
+++ scripts/time.js
@@ -0,0 +1,17 @@
+var startDate = new Date('2023-09-01');
+
+// Задаем дату, для которой нужно проверить неделю
+var currentDate = new Date();
+
+// Расчет количества недель между начальной датой и текущей датой
+var diffInWeeks = Math.floor((currentDate - startDate) / (7 * 24 * 60 * 60 * 1000));
+
+// Проверка, является ли неделя четной или нечетной
+if (diffInWeeks % 2 === 0) {
+  var weekType = 'вторая';
+} else {
+  var weekType = 'первая';
+}
+// Вывод результата
+console.log('Текущая неделя: ' + diffInWeeks);
+console.log('Тип недели: ' + weekType);
